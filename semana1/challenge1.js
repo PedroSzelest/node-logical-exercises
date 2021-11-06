68cd324d60ca2dc8da0c032a6c68cf0c15517abf
Pedro <pedro.szelest@gmail.com>
2021-11-05 23:01:01 -0500

    1 week done

--- semana1/challenge1.js
+++ semana1/challenge1.js
@@ -1,5 +1,12 @@
-const reverseString = (str) => {
-    //Implementación
+const reverseString = async (str) => {
+    if(typeof str !== "string" | str.length <= 1 | str.length > 15) throw new Error("Invalid string!")
+
+    try {
+        let res = str.split("").reverse().join("")
+        return res
+    } catch(error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
--- semana1/challenge3.js
+++ semana1/challenge3.js
@@ -1,6 +1,23 @@
-const capitalizeLetters = (str) => {
-    //Implementación
+const capitalizeLetters = async (str) => {
+    if(typeof str !== "string") throw new Error("Invalid string!")
+
+    try {
+        res = await str.split(" ")
+        console.log(res)
+        for(let c = 0; c < res.length; c++) {
+            res[c] = res[c][0].toUpperCase() + res[c].slice(1)
+            console.log(res[c])
+        }
+        res = res.join(" ")
+        
+        return res
+
+    } catch(error) {
+        console.log(error)
+    }
+
 }
+
 
 module.exports = {
     capitalizeLetters
